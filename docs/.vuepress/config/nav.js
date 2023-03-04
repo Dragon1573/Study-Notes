e97f73659fd55406fdf6a0387c91b69177ef1ea6
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2023-03-05 00:57:38 +0800

    配置更新（导航栏）：添加友链
    - Pinpe | 也许 ，http://pinpe.top/
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,5 +2,10 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '机器学习', link: '/ml/' },
-  { text: '技术杂谈', link: '/skill/' }
+  { text: '技术杂谈', link: '/skill/' },
+  {
+    text: '友链', items: [
+      { text: 'Pinpe | 也许', link: 'http://pinpe.top/' }
+    ]
+  }
 ]
