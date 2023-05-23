08e910940bbeedac7376aa32e5ceb38500b60ef6
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2023-05-23 23:04:51 +0800

    添加友链：冰白寒祭的博客
    - 顺带调整了友链顺序
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -5,8 +5,9 @@ module.exports = [
   { text: '技术杂谈', link: '/skill/' },
   {
     text: '友链', items: [
-      { text: 'Pinpe | 也许', link: 'http://pinpe.top/' },
-      { text: 'Qlittle - 一个笔记Blog', link: 'https://qlittlo.github.io/' }
+      { text: 'Qlittle - 一个笔记Blog', link: 'https://qlittlo.github.io/' },
+      { text: '冰白寒祭的博客', link: 'http://bingbaihanji.xyz/' },
+      { text: 'Pinpe | 也许', link: 'http://pinpe.top/' }
     ]
   }
 ]
